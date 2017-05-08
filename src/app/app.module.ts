84763dcadd39718663b549999feafb73f845a1b6
onwuzulike emeka <onwuzulike emeka>
2017-05-08 20:06:34 +0100

    added dashboard view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 
 import { AppComponent } from './layout/app.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { UploadComponent } from './upload/upload.component';
 import {ModalComponent} from './common/widgets/modalComponent/modal.component';
 import { ModalModule } from 'ngx-bootstrap';
 
@@ -12,15 +16,22 @@ import { ModalModule } from 'ngx-bootstrap';
   declarations: [
     AppComponent,
     GalleryComponent,
-    ModalComponent
+    ModalComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    RouterModule.forChild([
+            { path: '', redirectTo: 'gallery', pathMatch: 'full' },
+            { path: 'gallery', component: GalleryComponent },
+            { path: 'upload', component: UploadComponent },
+            { path: '**', redirectTo: 'gallery' }
+        ]),
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/layout/app.component.ts
+++ src/app/layout/app.component.ts
@@ -3,9 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template: `
-    <app-gallery [datasource]=images></app-gallery>
-  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
