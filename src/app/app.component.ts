f93ece926457e860d1a923e4002ab7fdf0fc8ae9
mokeam <jumokemariam@gmail.com>
2017-05-08 12:20:25 +0100

    Added Gallery Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `
+    <gallery [datasource]=images></gallery>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  images;
+ 
+   constructor(){
+      this.images = [
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"},
+	{"url":"https://goo.gl/AClrVV"}
+      ];
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
