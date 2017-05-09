8ba7e40a099219d1749567335b0f8648866e2e67
onwuzulike emeka <onwuzulike emeka>
2017-05-09 23:50:08 +0100

    added dynamic breadcrumb

--- src/app/layout/app.component.ts
+++ src/app/layout/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = "Gallery";
+export class AppComponent implements OnInit {
+  title: string;
+
+  constructor(private _router: Router) {}
+
+   private getTitle(path): string {
+        let value = '';
+
+        switch (path) {
+            case '/gallery':
+                value = 'Gallery';
+                break;
+
+            case '/upload':
+                value = 'Upload';
+                break;
+            default:
+                value = 'Current Page';
+        }
+
+        return value;
+   }
+
+   ngOnInit() {
+     this._router.events.subscribe((data: any) => {
+                this.title = this.getTitle(data.url);
+            });
+   }
+
 }
