fe4456d3ed0105bb3f3e89d1a2f2808ebecc59f6
cosmo1234 <harpheezsagaya@gmail.com>
2017-05-10 14:29:13 +0100

    Optimized load time

--- src/app/common/widgets/modalComponent/modal.component.ts
+++ src/app/common/widgets/modalComponent/modal.component.ts
@@ -3,7 +3,8 @@ import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
     selector: 'app-modal',
-    templateUrl: './modal.component.html'
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
     @ViewChild('modal')
